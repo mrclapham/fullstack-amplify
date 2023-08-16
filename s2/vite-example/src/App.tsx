8e1d7e74dba9d3ff66d109129f512c7634d71fec
Graham Clapham <grahamclapham@Grahams-MBP-2-2.broadband>
2023-08-16 10:38:38 +0100

    Initial commit

---
+++ s2/vite-example/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import styles from "./App.module.css";
+
+function App() {
+
+  return (
+      <div className={styles.container}>
+        <div className={styles.titleCtn}>
+          <h1>Rock, paper, scissors</h1>
+          <p>a React game</p>
+        </div>
+        </div>
+  )
+}
+
+export default App
