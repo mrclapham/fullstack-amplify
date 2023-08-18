5c4ea22e754a750fb02cc6ffd3925266f8e9e628
Graham Clapham <grahamclapham@Grahams-MBP-2-2.broadband>
2023-08-18 15:05:45 +0100

    api updating on the fly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,32 @@
 import './App.css';
+import { useEffect } from "react";
 import styles from "./App.module.css";
 
+const API_URL = import.meta.env.VITE_API_URL
+
+
 function App() {
+  useEffect(() => {
+    const getApi = async () => {
+      const response = await fetch(`${API_URL}test`);
+      const body = await response.json();
+      if (response.status !== 200) throw Error(body.message);
+      return body;
+    }
+
+    getApi()
+
+  }, []);
+
+  console.log(import.meta.env.VITE_API_URL) // 123
 
   return (
-      <div className={styles.container}>
-        <div className={styles.titleCtn}>
-          <h1>Rock, paper, scissors</h1>
-          <p>a React game</p>
-        </div>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.titleCtn}>
+        <h1>Rock, paper, scissors...</h1>
+        <p>a React game</p>
+      </div>
+    </div>
   )
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 dotenv.config()
 import { initDb } from './initDB';
 initDb();
 const app: Application = express();
+app.use(cors());
 
 const PORT: number = 5000;
+
+const DATABASE_URL: string = process.env.DATABASE_URL || 'mongodb://localhost:27017/test';
 
 app.get('/', (req: Request, res: Response): void => {
     res.json({message: 'Hello world!', env_name: process.env.NAME, other: 'other'});
@@ -13,7 +18,11 @@ app.get('/', (req: Request, res: Response): void => {
 
 
 app.get('/test', (req: Request, res: Response): void => {
-    res.json({message: 'Hello test!', env_name: process.env.NAME, other: 'more stuff'});
+    res.json({message: 'Hello test!', env_name: process.env.NAME, other: 'more stuff 123 xxx'});
+});
+
+app.post('/add', (req: Request, res: Response): void => {
+
 });
 
 app.listen(PORT, (): void => {
