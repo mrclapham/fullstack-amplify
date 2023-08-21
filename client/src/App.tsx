b4280792b1648fb6ad0b1f430e58ae83015195fb
Graham Clapham <grahamclapham@Grahams-MacBook-Pro-2.local>
2023-08-21 16:33:49 +0100

    Add display for products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,45 @@
 import './App.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 
 const API_URL = import.meta.env.VITE_API_URL
 
+type Product = {
+  id: string;
+  name: string;
+  price: number;
+}
 
-function App() {
+function App() {    
+  
+  const [products, setProducts] = useState<Product[]>([])
+
   useEffect(() => {
     const getApi = async () => {
-      const response = await fetch(`${API_URL}test`);
+      const response = await fetch(`${API_URL}products`);
       const body = await response.json();
       if (response.status !== 200) throw Error(body.message);
       return body;
     }
-
-    getApi()
-
+    getApi().then(res => {
+      console.log(res);
+      setProducts(res);
+    })
   }, []);
-
-  console.log(import.meta.env.VITE_API_URL) 
 
   return (
     <div className={styles.container}>
       <div className={styles.titleCtn}>
         <h1>Rock, paper, scissors...</h1>
-        <p>a React game</p>
+        {JSON.stringify(products)}
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <h2>{product.name}</h2>
+              <p>{product.price}</p>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
