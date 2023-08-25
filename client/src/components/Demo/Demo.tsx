2ef2a5162c198a8242a541f261ae44891b8b148f
Graham Clapham <grahamclapham@grahams-mbp-2.home>
2023-08-25 16:55:43 +0100

    more UI updates

--- client/src/components/Demo/Demo.tsx
+++ client/src/components/Demo/Demo.tsx
@@ -1,3 +1,7 @@
 export const Demo = () => {
-    return (<h1>DEMO 123</h1>)
+    return (<><h1>Mern app</h1>
+    <p>Type a name and price and click the button. The items will be added to the database.</p>
+    </>
+    
+    )
 };
